e734e64c7952ec18ca60a7d76d0f86d28e94e05d
Shadrack kipkorir <shadybett540@gmail.com>
2023-09-26 13:17:31 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const names = ["Guadalupe","Ollie","Aki"]
+const eventName = ["birthday"]
+function writeCards(names,eventName) 
+{const thankYouMessages = []
+    for(let i =0;i<names.length;i++){
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessages;
+}
+function countDown(number) {
+    while (number >=0) {
+        console.log (number);
+        number --;
+    }
+}
